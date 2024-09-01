d5029e1b7aa789ce0a557d3e629800d3aecc29b5
cfitz3 <165530834+cfitz3@users.noreply.github.com>
2024-09-01 11:47:34 +0100

    added support for new dedicated host

--- API/utils/redisClient.js
+++ API/utils/redisClient.js
@@ -5,11 +5,11 @@ const processedMessageIds = new Set();
 
 // Create Redis clients for publishing and subscribing
 const redisPublisherClient = redis.createClient({
-  url: 'redis://5.161.243.233:6379'
+  url: 'redis://195.201.242.60:6379'
 });
 
 const redisSubscriberClient = redis.createClient({
-  url: 'redis://5.161.243.233:6379'
+  url: 'redis://195.201.242.60:6379'
 });
 
 // Error handling for publisher client
